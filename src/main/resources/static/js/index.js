6e34d1894d77e63496ce2acc723347fb4638df22
박세준 <sjpark@selab.co.kr>
2022-05-18 10:56:55 +0900

    [ETC] 모듈링 세미나 준비

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -4,8 +4,10 @@
 // document.body.appendChild(div);
 // setInterval(() => (div.innerText = currentTime().toString()), 1000);
 
-import {helloWorld, helloWorld2} from "./test.js";
-
-console.log(helloWorld2())
+// import {helloWorld, helloWorld2} from "./test.js";
+//
+// console.log(helloWorld2())
 
 // console.log(APIKEY)
+
+console.log("모듈링 연습")
